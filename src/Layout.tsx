ff9e13de984696a2a7ac7996a8b381cc3b451485
bthomas <ben.thomas@gamma.co.uk>
2025-03-21 17:13:09 +0000

    Make it so that you can be whoever you want to be when you are a tester

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,10 +3,10 @@ import { signOut } from "aws-amplify/auth";
 import { useNavigate } from "react-router-dom";
 import { useUserAttributes } from "./PermissionsProvider/UserAttributesContext.tsx"
 import { Button } from "./Styles/Styles.tsx"
-import { User } from "./Constants/constants.ts";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const thisUser: User = useUserAttributes();
+
+    const { user } = useUserAttributes(); // Extract 'user' instead of using the whole object
     const navigate = useNavigate();
 
     const handleLogout = async () => {
@@ -21,7 +21,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <div>
             <header style={{ display: "flex", justifyContent: "space-between", padding: "1rem 2rem", background: "#f5f5f5", gap: "3rem"}}>
-                <h1>Hello, {thisUser.name}</h1>
+                <h1>Hello, {user ? user.name : "unexpected guest"}</h1>
                 <Button onClick={handleLogout} isOpen={true}>
                     Logout
                 </Button>
--- src/UserMenus/Tester/TesterMenu.tsx
+++ src/UserMenus/Tester/TesterMenu.tsx
@@ -1,17 +1,30 @@
 import { Profile } from "../../Constants/constants.ts";
+import { useUserAttributes } from "../../PermissionsProvider/UserAttributesContext.tsx";
+import { useNavigate } from "react-router-dom";
 
 const TesterMenu = () => {
+    const { changeUserAttributes } = useUserAttributes(); // Get changeUserAttributes from context
+    const navigate = useNavigate();
+
+    function handleNameChange(role: string) {
+        changeUserAttributes({ profile: role as Profile, name: 'Gobshite' });
+        console.log('You fucken druggo')
+        navigate("/");
+    }
+
     return (
         <main>
             <h1>Hello, this is the page for you, the tester</h1>
             <h2>When you log in you will be able to impersonate all roles</h2>
             <ul>
                 {Object.values(Profile).map((role) => (
-                    <li key={role as string}>{role as string}</li>
+                    <li key={role as string} onClick={() => handleNameChange(role)}>
+                        {role as string}
+                    </li>
                 ))}
             </ul>
         </main>
     );
 };
 
-export default TesterMenu
+export default TesterMenu;
