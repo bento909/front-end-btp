9b7bb68a380d01141f93ed869ed6e62d3d859022
bthomas <ben.thomas@gamma.co.uk>
2025-03-24 12:50:47 +0000

    Let's give our fetch users thing a go

---
+++ src/UserMenus/Admin/MainMenu.tsx
@@ -0,0 +1,16 @@
+import SignUp from '../../Components/CreateUser';
+import ViewAllUsers from "../../Components/ViewUsers.tsx";
+
+const MainMenu = () => {
+    return (
+        <main>
+            <ul>
+                <li>Todo More stuff</li>
+            </ul>
+            <ViewAllUsers/>
+            <SignUp/>
+        </main>
+    );
+};
+
+export default MainMenu
