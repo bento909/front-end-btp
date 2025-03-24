d167e8ddb4cd3f789466e60827698cdabde87dfa
bthomas <ben.thomas@gamma.co.uk>
2025-03-24 23:59:45 +0000

    Revert "Why am I not being redirected to Tester Menu????"
    This reverts commit 81bd2f55f6ab5071a33ecb70b53b51affd69efab.

--- src/Pages/Page/PostLoginScreen.tsx
+++ src/Pages/Page/PostLoginScreen.tsx
@@ -4,25 +4,20 @@ import { Profile } from '../../Constants/constants.tsx';
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store.tsx";
 
-const user = useSelector((state: RootState) => state.auth.user);
-
 const PostLoginScreen = () => {
+    const user = useSelector((state: RootState) => state.auth.user);
     const navigate = useNavigate();
     useEffect(() => {
-        if (user) {
-            const handleUserLogin = async () => {
-                const role = user ? user.profile : Profile.BASIC_USER
-                console.log('my role is ' + role)
-                if (role === Profile.TESTER) {
-                    console.log('navigating to tester menu')
-                    navigate('/testerMenu');
-                } else {
-                    console.log('navigating to admin menu')
-                    navigate('/adminMenu');
-                }
-            };
-            handleUserLogin();
-        }
+        const handleUserLogin = async () => {
+            const role = user ? user.profile : Profile.BASIC_USER
+            console.log('my role is ' + role)
+            if (role === Profile.TESTER) {
+                navigate('/testerMenu');
+            } else  {
+                navigate('/adminMenu');
+            }
+        };
+        handleUserLogin();
     }, [navigate, user]);
 
     return <div>Loading...</div>;
