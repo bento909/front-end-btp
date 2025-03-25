be5e8c0648b5338a539f61bf50130fa48c44e2d1
bthomas <ben.thomas@gamma.co.uk>
2025-03-25 09:31:32 +0000

    Add Comment

--- src/Pages/Components/ViewUsers.tsx
+++ src/Pages/Components/ViewUsers.tsx
@@ -12,6 +12,8 @@ interface GetUserListProps {
 }
 
 const UserList: React.FC<GetUserListProps> = () => {
+    //TODO even though you might be in test mode, the Lambda that fetches the users doesn't know that - therefore we need
+    //to ensure that the userlist loads user as expected for all cases.
     const { users, loading, error } = useSelector((state: RootState) => state.users);
 
     if (loading) return <div>Loading users...</div>;
